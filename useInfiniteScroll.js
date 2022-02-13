8388f49b3838b37725cb55b6a5f10b05011975d3
Koleci Rei <rei.koleci@sisal.it>
2022-02-13 13:37:48 +0100

    useInfiniteScroll Hook

---
+++ useInfiniteScroll.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+
+const useInfiniteScroll = () => {
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        window.addEventListener(
+            "scroll",
+            () => {
+                const {
+                    scrollTop,
+                    scrollHeight,
+                    clientHeight
+                } = document.documentElement;
+
+                /* Magic code from THE internet */
+                if (scrollTop + clientHeight >= scrollHeight - 5) {
+                    setPage(page => page + 1); 
+                }
+            },
+            {
+                passive: true
+            }
+        );
+    }, [page]);
+
+    return {
+        page
+    };
+};
+
+export default useInfiniteScroll
